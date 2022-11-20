198dbdd95112e1dd4fd719bc3ccf4fb992fe271a
kyle <97553364+kkarpysh@users.noreply.github.com>
2022-11-20 14:48:02 -0700

    Update load-json.py
    skeleton code

---
+++ load-json.py
@@ -0,0 +1,31 @@
+import pymongo
+import json
+
+# === GET JSON FILE NAME AND PORT ===
+filename = input("Enter the json filename: ") # will need to handle errors l8tor
+portnum = int(input("Enter the mongodb port number: "))
+
+# === INIT DATABASE ===
+client = pymongo.MongoClient("??????" + str(portnum))
+
+dblist = client.list_database_names()
+if "291db" not in dblist:
+    db = client["291db"]
+else:
+    print("database exists")
+
+# === INIT COLLECTION ===
+collist = db.list_collection_names()
+if "dblp" in collist:
+    collection = db["dblp"]
+    collection.drop()
+
+collection = db["dblp"]
+
+# === LOAD JSON INTO COLLECTION ===
+filename = filename + ".json"
+jsonfile = open(filename, 'r')
+
+for jsonObj in jsonfile:
+    data = json.loads(jsonObj)
+    collection.insert_one(data) # should insert line by line?
