1738d52ff9e97dcf963a7ef10c9bc6d71519a63e
Adam Boman <aboman@cs.ualberta.ca>
2022-11-22 13:57:46 -0700

    Search For Articles Done

--- database.py
+++ database.py
@@ -15,13 +15,63 @@ def connect_to_db():
     return
 
 def search_for_articles(keywords):
+    # only works for one keyword right now
     global db, client, collection
     keywords = list(keywords)
+    query = []
+    if len(keywords) == 1:
+        query.append({'authors': re.compile(keywords[0], re.IGNORECASE)})
+        query.append({'title': re.compile(keywords[0], re.IGNORECASE)})
+        query.append({'abstract': re.compile(keywords[0], re.IGNORECASE)})
+        query.append({'venue': re.compile(keywords[0], re.IGNORECASE)})
+        query.append({'year': re.compile(keywords[0], re.IGNORECASE)})
+        articles = collection.find({'$or' : query})
+        return articles
+    else:
+        for keyword in keywords:
+            temp_query = []
+            temp_query2 = {}
+            temp_query.append({'authors': re.compile(keyword, re.IGNORECASE)})
+            temp_query.append({'title': re.compile(keyword, re.IGNORECASE)})
+            temp_query.append({'abstract': re.compile(keyword, re.IGNORECASE)})
+            temp_query.append({'venue': re.compile(keyword, re.IGNORECASE)})
+            temp_query.append({'year': re.compile(keyword, re.IGNORECASE)})
+            temp_query2['$or'] = temp_query
+            query.append(temp_query2)
+        articles = collection.find({'$and': query})
+        return articles
+
+
+def search_for_authors(keywords):
+    global db, client, collection
+    keywords = list(keywords)
+    publication_count = []
     
-    articles = collection.find({'$and':[
-                                    {'authors': re.compile(keywords[0], re.IGNORECASE)},
-                                    {'title': re.compile(keywords[0], re.IGNORECASE)}
-                                ]})
-    for article in articles:
-        print(article) 
-    return articles
+    authors = collection.find({}, {'authors': re.compile(keywords[0], re.IGNORECASE)})
+    
+    for author in authors:
+        count = collection.countDocuments({}, {'authors': author})
+        publication_count.append(count)
+
+    return authors, publication_count
+
+def get_author_details(author_name):
+    global db, client, collection
+    author = collection.find({}, {'authors': re.compile(author_name, re.IGNORECASE)})
+    
+    author_details = collection.find({}, {'title': {'authors': author},
+                                          'year': {'authors': author},
+                                          'venue': {'authors': author}
+                                         }).sort({'year': -1})
+    return author_details
+
+def get_referencing_articles(article):
+    global db, client, collection
+    article_id = article['id']
+    references = collection.find({'references': article_id})
+    referencing_articles = []
+    for item in references:
+        referencing_articles.append(item)
+    if len(referencing_articles) == 0:
+        return None
+    return referencing_articles
--- main.py
+++ main.py
@@ -3,6 +3,91 @@ import database
 import os
 
 
+def more_article_information(article):
+    referencing_articles = database.get_referencing_articles(article)
+    clear_terminal()
+    # Abstract
+    try:
+        print("Abstract:")
+        print(article['abstract'])
+    except:
+        print('Abstract Not Found.')
+    
+    # Authors
+    try:
+        print("\nAuthors:")
+        print(article['authors'])
+    except:
+        print('Author(s) Not Found.')
+
+    # Number of Citations
+    try:
+        print('\nNumber of Citations:', article['n_citation'])
+    except:
+        print('Number of Citations Not Found')
+
+    # References
+    try:
+        print('\nReferences:')
+        print(article['references'])
+    except:
+        print('Reference(s) Not Found.')
+
+    # title
+    try:
+        print('\nTitle:', article['title'])
+    except:
+        print('Title Not Found.')
+
+    # venue
+    try:
+        print('\nVenue:', article['venue'])
+    except:
+        print('Venue Not Found.')
+
+    # year 
+    try:
+        print('\nYear:', article['year'])
+    except:
+        print('Year Not Found.')
+
+    # id
+    try:
+        print('\nID:', article['id'])
+    except:
+        print('ID Not Found.')
+
+    # referencing articles
+    if referencing_articles != None:
+        print('\nOther Articles That Reference This Article:')
+        for article in referencing_articles:
+            print('ID: ' + str(article['id']) + ', Title: ' + str(article['title']) + ', Year: ' + str(article['year']) + '\n')
+    input('Type Enter To Continue.')
+    return
+
+
+def article_menu(articles):
+    index = []
+    counter = 1
+    clear_terminal()
+    temp_list = []
+    for article in articles:
+        temp_list.append(article)
+        print(str(counter) + '.', article['id'], article['title'], article['year'], article['venue'])
+        index.append(str(counter))
+        counter += 1
+    if len(index) != 0:
+        user_in = ''
+        while user_in not in index:
+            user_in = input('\nType Menu to Return.\nSelect an Article to See More Information. ')
+            if user_in.upper() == 'MENU':
+                return 
+        more_article_information(temp_list[int(user_in) - 1])
+        return 
+    else:
+        input('No Results Found.\nPress Enter to Continue.')
+        return
+
 def search_for_articles():
     clear_terminal()
     user_keywords = ''
@@ -12,16 +97,60 @@ def search_for_articles():
         user_keywords = input('Enter Keywords: ')
     keywords = user_keywords.split() # splits the input by spaces
     keywords = set(keywords) # gets rid of duplicates
-    results = database.search_for_articles(keywords)
+    articles = database.search_for_articles(keywords)
+    return articles
 
+def search_for_authors():
+    clear_terminal()
+    user_keywords = ''
 
+    print("Search For Authors:")
+    while user_keywords == '' or user_keywords.isspace():
+        user_keywords = input("Enter Keywords: ")
+    keywords = user_keywords.split()
+    keywords = set(keywords)
+    
+    authors, pub_count = database.search_for_authors(keywords)
+    
+    # === display authors ===
+    i = 0
+    for author in authors:
+        print(str(i) + ". Name: " + author + " Publication Count: " + str(pub_count[i]))
+        i += 1
+    
+    # === select an author ===
+    user_in = ''
+    while user_in.lower() not in ['y', 'n']:
+        user_in = input("Would you like to select an author? (Y/N): ")
+    
+    if user_in.lower() == 'y':
+        # convert list to be workable
+        authors_lower = []
+        for author in authors:
+            authors_lower.append(author.lower())
+   
+        user_in = ''
+        while user_in.lower() not in authors_lower:
+            user_in = input("Type an author's name: ")
+    
+        # === get selected author details ===
+        author_details = database.get_author_details(user_in)
+        
+        # === display the details ===
+        clear_terminal()
+        print(str(user_in.upper()) + ":") # print the author name at the top
+        for i in range(len(author_details)):
+            print("Title: " + str(author_details[0][i]) + "\n"
+                  "Year: " + str(author_details[1][i]) + "\n"
+                  "Venue: " + str(author_details[2 ][i] + "\n\n"))
+    elif user_in.lower() == 'n':
+        return
 
-    return
 
 
 def main_menu():
     user_in = ''
-    while user_in not in ['1','2','3','4','5']:
+    while user_in not in ['5']:
         clear_terminal()
         print('Main Menu:')
         print('1. Search for Articles')
@@ -31,10 +160,11 @@ def main_menu():
         print('5. Exit')
         user_in = input('Select an Option: ')
         if user_in == '1': # Search for Articles
-            search_for_articles()
+            articles = search_for_articles()
+            article_menu(articles)
             pass
         elif user_in == '2': # Search for Authors
-            pass
+            search_for_authors()
         elif user_in == '3': # List the Venues
             pass
         elif user_in == '4': # Add an Article
