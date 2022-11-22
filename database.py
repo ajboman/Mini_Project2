129bba70c9968d7812fdf908f4ffb7233dabad6b
kyle <97553364+kkarpysh@users.noreply.github.com>
2022-11-21 17:34:51 -0700

    Update database.py
    more untested code

--- database.py
+++ database.py
@@ -25,3 +25,25 @@ def search_for_articles(keywords):
     for article in articles:
         print(article) 
     return articles
+
+def search_for_authors(keywords):
+    global db, client, collection
+    keywords = list(keywords)
+    publication_count = []
+    
+    authors = collection.find({}, {'authors': re.compile(keywords[0], re.IGNORECASE)})
+    
+    for author in authors:
+        count = collection.countDocuments({}, {'authors': author})
+        publication_count.append(count)
+
+    return authors, publication_count
+
+def get_author_details(author_name):
+    author = collection.find({}, {'authors': re.compile(author_name, re.IGNORECASE)})
+    
+    author_details = collection.find({}, {'title': {'authors': author},
+                                          'year': {'authors': author},
+                                          'venue': {'authors': author}
+                                         }).sort({'year': -1})
+    return author_details
